c60b41a96fb07b87655a29f7024b5dc428c649ea
Max <max.gavr@tut.by>
2017-06-05 23:20:45 +0300

    Refactor code, draw charts based on one data set

--- simd.js
+++ simd.js
@@ -10,8 +10,8 @@ var SUBTRACTION_TIME;
 var ABS_TIME;
 var COMPARE_TIME;
 
-var T1 = 0;
-var Tn = 0;
+var T1;
+var Tn;
 
 var p, m, q;
 var n;
@@ -31,8 +31,6 @@ var operationRanks = {
     secondIfTrue: 0, // multiplication + addition
     secondIfFalse: 0 // two multiplication + module + subtraction
 }
-
-
 
 function generateMatrix(x, y, empty = false){
 
@@ -65,32 +63,33 @@ function formatMatrix(A, precision = 2){
     return result;
 }
 
-function printMatrixToTable(id, matrix){
-    var title = id.charAt(id.length - 1);
-    var x = matrix.length;
-    var y = matrix[0].length;
+function printResults(A, B, C){
 
-    var table = document.getElementById(id);
-    table.innerHTML = "";
-    var currentRow, currentCell;
-    for (var i = 0; i < x + 1; i++){
-        currentRow = table.insertRow();
-        for (var j = 0; j < y + 1; j++){
-            currentCell = currentRow.insertCell();
-            if (i > 0 && i < x && j > 0 && j < y){
-                currentCell.innerHTML = matrix[i][j];
-            } else if (i == 0 && j == 0){
-                currentCell.innerHTML = "<b>" + title + "</b>";
-            } else if (i == 0 || j == 0){
-                currentCell.innerHTML = "";
-            } else if (i == x || j == y){
-                currentCell.innerHTML = matrix[i-1][j-1];
+    function printMatrixToTable(id, matrix){
+        var title = id.charAt(id.length - 1);
+        var x = matrix.length;
+        var y = matrix[0].length;
+
+        var table = document.getElementById(id);
+        table.innerHTML = "";
+        var currentRow, currentCell;
+        for (var i = 0; i < x + 1; i++){
+            currentRow = table.insertRow();
+            for (var j = 0; j < y + 1; j++){
+                currentCell = currentRow.insertCell();
+                if (i > 0 && i < x && j > 0 && j < y){
+                    currentCell.innerHTML = matrix[i][j];
+                } else if (i == 0 && j == 0){
+                    currentCell.innerHTML = "<b>" + title + "</b>";
+                } else if (i == 0 || j == 0){
+                    currentCell.innerHTML = "";
+                } else if (i == x || j == y){
+                    currentCell.innerHTML = matrix[i-1][j-1];
+                }
             }
         }
     }
-}
 
-function printResults(A, B, C){
     printMatrixToTable("tableA", A);
     printMatrixToTable("tableB", B);
     printMatrixToTable("tableC", formatMatrix(C));
@@ -103,73 +102,13 @@ function getParameters(){
     m = document.getElementById("input_m").value;
     q = document.getElementById("input_q").value;
     n = document.getElementById("input_n").value;
+    CHART_RANGE = document.getElementById("input_chart_range").value;
 
-    ADDITION_TIME = parseInt(document.getElementById("input_t1").value);
-    SUBTRACTION_TIME = parseInt(document.getElementById("input_t2").value);
-    MULTIPLICATION_TIME = parseInt(document.getElementById("input_t3").value);
-    ABS_TIME = parseInt(document.getElementById("input_t4").value);
-    COMPARE_TIME = parseInt(document.getElementById("input_t5").value);
-}
-
-function countOperations(amount){
-    totalOperations.addition += amount.addition;
-    totalOperations.multiplication += amount.multiplication;
-    totalOperations.subtraction += amount.subtraction;
-    totalOperations.comparison += amount.comparison;
-    totalOperations.absolute += amount.absolute;
-}
-
-function calculateTime(amount){
-    var mul = amount.multiplication * MULTIPLICATION_TIME;
-    var sum = amount.addition * ADDITION_TIME;
-    var sub = amount.subtraction * SUBTRACTION_TIME;
-    var comp = amount.comparison * COMPARE_TIME;
-    var abs = amount.absolute * ABS_TIME;
-
-    var additionalSums = (m - 1)* ADDITION_TIME;
-
-    return mul + sum + sub + comp + abs + additionalSums;
-}
-
-function calculateParallelTime(){
-    var T = 0;
-
-    T += calculateParallelStages(operationRanks.firstIf) * ABS_TIME;
-    T += calculateParallelStages(operationRanks.firstIf) * ABS_TIME;
-    T += calculateParallelStages(operationRanks.firstIf) * COMPARE_TIME;
-    T += calculateParallelStages(operationRanks.firstIfTrue) * MULTIPLICATION_TIME;
-    T += calculateParallelStages(operationRanks.secondIf) * COMPARE_TIME;
-    T += calculateParallelStages(operationRanks.secondIfTrue) * MULTIPLICATION_TIME;
-    T += calculateParallelStages(operationRanks.secondIfTrue) * ADDITION_TIME;
-    T += calculateParallelStages(operationRanks.secondIfFalse) * MULTIPLICATION_TIME;
-    T += calculateParallelStages(operationRanks.secondIfFalse) * MULTIPLICATION_TIME;
-    T += calculateParallelStages(operationRanks.secondIfFalse) * ABS_TIME;
-    T += calculateParallelStages(operationRanks.secondIfFalse) * SUBTRACTION_TIME;
-
-    return T;
-}
-
-function calculateParallelStages(amountOfOperations){
-    if (n > 1){
-        var stages = Math.ceil(amountOfOperations / n);
-    }
-    else{
-        stages = amountOfOperations;
-    }
-    return stages;
-}
-
-function calculateDivergency(){
-    var Lavg = (operationRanks.firstIf * COMPARE_TIME * 2+
-                operationRanks.firstIf * 2 * ABS_TIME +
-               operationRanks.firstIfTrue * MULTIPLICATION_TIME * 2+
-               operationRanks.secondIf * COMPARE_TIME * 2+
-               operationRanks.secondIfTrue * (MULTIPLICATION_TIME + ADDITION_TIME) * 2+
-               operationRanks.secondIfFalse * (2 * MULTIPLICATION_TIME + SUBTRACTION_TIME * COMPARE_TIME) * 2 +
-               operationRanks.secondIfFalse * ABS_TIME +
-               calculateParallelStages(m - 1) * p * q * ADDITION_TIME * 4) / (2 * m * p * q);
-    var Lsum = Tn;
-    return Lsum / Lavg;
+    ADDITION_TIME = document.getElementById("input_t1").value;
+    SUBTRACTION_TIME = document.getElementById("input_t2").value;
+    MULTIPLICATION_TIME = document.getElementById("input_t3").value;
+    ABS_TIME = document.getElementById("input_t4").value;
+    COMPARE_TIME = document.getElementById("input_t5").value;
 }
 
 function reset(){
@@ -189,66 +128,113 @@ function reset(){
     operationRanks.secondIfFalse = 0;
 }
 
+function calculateTime(){
+    var mul = totalOperations.multiplication * MULTIPLICATION_TIME;
+    var sum = totalOperations.addition * ADDITION_TIME;
+    var sub = totalOperations.subtraction * SUBTRACTION_TIME;
+    var comp = totalOperations.comparison * COMPARE_TIME;
+    var abs = totalOperations.absolute * ABS_TIME;
+
+    var additionalSums = (m - 1) * p * q * ADDITION_TIME;
+
+    return mul + sum + sub + comp + abs + additionalSums;
+}
+
+function calculateParallelTime(){
+    var T = 0;
+
+    T += calculateParallelStages(operationRanks.firstIf * 2) * ABS_TIME;
+    T += calculateParallelStages(operationRanks.firstIf) * COMPARE_TIME;
+
+    T += calculateParallelStages(operationRanks.firstIfTrue) * MULTIPLICATION_TIME;
+
+    T += calculateParallelStages(operationRanks.secondIf) * COMPARE_TIME;
+
+    T += calculateParallelStages(operationRanks.secondIfTrue) * MULTIPLICATION_TIME;
+    T += calculateParallelStages(operationRanks.secondIfTrue) * ADDITION_TIME;
+
+    T += calculateParallelStages(operationRanks.secondIfFalse * 2) * MULTIPLICATION_TIME;
+    T += calculateParallelStages(operationRanks.secondIfFalse) * ABS_TIME;
+    T += calculateParallelStages(operationRanks.secondIfFalse) * SUBTRACTION_TIME;
+
+    T += calculateParallelStages(m - 1) * p * q * ADDITION_TIME;
+
+    return T;
+}
+
+function calculateParallelStages(amountOfOperations){
+    if (n > 1){
+        var stages = Math.ceil(amountOfOperations / n);
+    }
+    else{
+        stages = amountOfOperations;
+    }
+    return stages;
+}
+
+function calculateDivergency(){
+    var Lavg = (operationRanks.firstIf * COMPARE_TIME  * 2 +
+               operationRanks.firstIf * 2 * ABS_TIME +
+               operationRanks.firstIfTrue * MULTIPLICATION_TIME * 2 +
+               operationRanks.secondIf * COMPARE_TIME * 2 +
+               operationRanks.secondIfTrue * (MULTIPLICATION_TIME + ADDITION_TIME) * 2 +
+               operationRanks.secondIfFalse * (2 * MULTIPLICATION_TIME + SUBTRACTION_TIME * COMPARE_TIME) * 2 +
+               operationRanks.secondIfFalse * ABS_TIME +
+               calculateParallelStages(m - 1) * p * q * ADDITION_TIME * 4) / (2 * m * p * q);
+
+    var Lsum = Tn;
+    return Lsum / Lavg;
+}
+
 function calculate(A, B){
-    var C = generateMatrix(p, q, true);
     reset();
+    var C = generateMatrix(p, q, true);
 
     for (var i = 0; i < p; i++){
         for (var j = 0; j < q; j++){
             var c = 0;
-
-            var amount = {
-                addition: 0,
-                subtraction: 0,
-                multiplication: 0,
-                absolute: 0,
-                comparison: 0
-            };
 
             for (var k = 0; k < m; k++){
                 var a = A[i][k];
                 var b = B[k][j];
                 var d = 0;
 
-                amount.comparison += 1;
-                amount.absolute += 2;
+                totalOperations.comparison += 1;
+                totalOperations.absolute += 2;
                 operationRanks.firstIf += 1;
                 if (Math.abs(a) <= Math.abs(b)){
                     d = a * b;
 
-                    amount.multiplication += 1;
+                    totalOperations.multiplication += 1;
                     operationRanks.firstIfTrue += 1;
                 }
                 else{
-                    amount.comparison += 1;
+                    totalOperations.comparison += 1;
                     operationRanks.secondIf += 1;
                     if (a == 0){
                         d = a * a + b;
 
-                        amount.multiplication += 1;
-                        amount.addition += 1;
+                        totalOperations.multiplication += 1;
+                        totalOperations.addition += 1;
                         operationRanks.secondIfTrue += 1;
                     }
                     else{
                         d = a * a - Math.abs(a * b);
 
-                        amount.multiplication += 2;
-                        amount.absolute += 1;
-                        amount.subtraction += 1;
+                        totalOperations.multiplication += 2;
+                        totalOperations.absolute += 1;
+                        totalOperations.subtraction += 1;
                         operationRanks.secondIfFalse += 1;
                     }
                 }
                 c += d;
             }
-
             C[i][j] = c;
-            T1 += calculateTime(amount);
-            countOperations(amount);
         }
     }
 
-    Tn += calculateParallelTime();
-    Tn += calculateParallelStages(m - 1) * p * q * ADDITION_TIME;
+    T1 = calculateTime();
+    Tn = calculateParallelTime();
     return C;
 }
 
@@ -264,88 +250,39 @@ function process(){
 
 function drawCharts(){
     getParameters();
-    getTimes();
 
-    drawChart1();
-    drawChart2();
-    drawChart3();
-    drawChart4();
-    drawChart5();
-    drawChart6();
+    var A = generateMatrix(p, CHART_RANGE);
+    var B = generateMatrix(CHART_RANGE, q);
+
+    drawChart1(A, B);
+    drawChart2(A, B);
+    drawChart3(A, B);
+    drawChart4(A, B);
+    drawChart5(A, B);
+    drawChart6(A, B);
 }
 
-function drawChart1() {
+// Ky(n)
+function drawChart1(A, B) {
     google.charts.load('current', {'packages':['line']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
         var data = new google.visualization.DataTable();
-
-        var rows = [];
-
-        data.addColumn('number', 'r');
-
-        for (var i = 1; i <= CHART_RANGE; i++) {
-            rows.push([i]);
-            data.addColumn('number', 'n = ' + i);
-
-            n = i;
-
-            for (var j = 1; j <= CHART_RANGE; j++) {
-                m = j;
-
-                var A = generateMatrix(p, m);
-                var B = generateMatrix(m, q);
-                var C = calculate(A, B);
-
-                rows[i - 1].push(T1 / Tn);
-            }
-        }
-        data.addRows(rows);
-
-        var options = {
-            chart: {
-                title: 'График Ky(n,r) от ранга задачи'
-            },
-            hAxis: {
-                title: "Ранг задачи r"
-            },
-            vAxis: {
-                title: "Коэффициент ускорения Ку"
-            },
-            width: CHART_WIDTH,
-            height: CHART_HEIGHT
-        };
-
-        var chart = new google.charts.Line(document.getElementById('chart_Ky_r'));
-        chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-}
-
-function drawChart2() {
-    google.charts.load('current', {'packages':['line']});
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-        var data = new google.visualization.DataTable();
-
         var rows = [];
 
         data.addColumn('number', 'n');
+        for (var c = 1; c <= CHART_RANGE; c++){
+            data.addColumn('number', 'r = ' + c);
+        }
 
-        for (var i = 1; i <= CHART_RANGE; i++) {
-            rows.push([i]);
-            data.addColumn('number', 'r = ' + i);
+        for (var i = 1; i <= CHART_RANGE; i++){
+            n = i;
+            rows.push([n]);
 
-            m = i;
-
-            for (var j = 1; j <= CHART_RANGE; j++) {
-                n = j;
-
-                var A = generateMatrix(p, m);
-                var B = generateMatrix(m, q);
+            for (var j = 1; j <= CHART_RANGE; j++){
+                m = j;
                 var C = calculate(A, B);
-
                 rows[i - 1].push(T1 / Tn);
             }
         }
@@ -370,30 +307,73 @@ function drawChart2() {
     }
 }
 
-function drawChart3() {
+// Ky(r)
+function drawChart2(A, B) {
     google.charts.load('current', {'packages':['line']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
         var data = new google.visualization.DataTable();
+        var rows = [];
 
+        data.addColumn('number', 'r');
+        for (var c = 1; c <= CHART_RANGE; c++){
+            data.addColumn('number', 'n = ' + c);
+        }
+
+        for (var i = 1; i <= CHART_RANGE; i++){
+            m = i;
+            rows.push([m]);
+
+            for (var j = 1; j <= CHART_RANGE; j++){
+                n = j;
+
+                var C = calculate(A, B);
+                rows[i - 1].push(T1 / Tn);
+            }
+        }
+        data.addRows(rows);
+
+        var options = {
+            chart: {
+                title: 'График Ky(n,r) от ранга задачи'
+            },
+            hAxis: {
+                title: "Ранг задачи r"
+            },
+            vAxis: {
+                title: "Коэффициент ускорения Ку"
+            },
+            width: CHART_WIDTH,
+            height: CHART_HEIGHT
+        };
+
+        var chart = new google.charts.Line(document.getElementById('chart_Ky_r'));
+        chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+}
+
+// e(n)
+function drawChart3(A, B) {
+    google.charts.load('current', {'packages':['line']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = new google.visualization.DataTable();
         var rows = [];
 
         data.addColumn('number', 'n');
+        for (var c = 1; c <= CHART_RANGE; c++){
+            data.addColumn('number', 'r = ' + c);
+        }
 
-        for (var i = 1; i <= CHART_RANGE; i++) {
-            rows.push([i]);
-            data.addColumn('number', 'r = ' + i);
+        for (var i = 1; i <= CHART_RANGE; i++){
+            n = i;
 
-            m = i;
-
-            for (var j = 1; j <= CHART_RANGE; j++) {
-                n = j;
-
-                var A = generateMatrix(p, m);
-                var B = generateMatrix(m, q);
+            rows.push([n]);
+            for (var j = 1; j <= CHART_RANGE; j++){
+                m = j;
                 var C = calculate(A, B);
-
                 rows[i - 1].push(T1 / Tn / n);
             }
         }
@@ -418,30 +398,28 @@ function drawChart3() {
     }
 }
 
-function drawChart4() {
+// e(r)
+function drawChart4(A, B) {
     google.charts.load('current', {'packages':['line']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
         var data = new google.visualization.DataTable();
-
         var rows = [];
 
         data.addColumn('number', 'r');
+        for (var c = 1; c <= CHART_RANGE; c++){
+            data.addColumn('number', 'n = ' + c);
+        }
 
-        for (var i = 1; i <= CHART_RANGE; i++) {
-            rows.push([i]);
-            data.addColumn('number', 'n = ' + i);
+        for (var i = 1; i <= CHART_RANGE; i++){
+            m = i;
+            rows.push([m]);
 
-            n = i;
+            for (var j = 1; j <= CHART_RANGE; j++){
+                n = j;
 
-            for (var j = 1; j <= CHART_RANGE; j++) {
-                m = j;
-
-                var A = generateMatrix(p, m);
-                var B = generateMatrix(m, q);
                 var C = calculate(A, B);
-
                 rows[i - 1].push(T1 / Tn / n);
             }
         }
@@ -466,30 +444,27 @@ function drawChart4() {
     }
 }
 
-function drawChart5() {
+// D(n)
+function drawChart5(A, B) {
     google.charts.load('current', {'packages':['line']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
         var data = new google.visualization.DataTable();
-
         var rows = [];
 
         data.addColumn('number', 'n');
+        for (var c = 1; c <= CHART_RANGE; c++){
+            data.addColumn('number', 'r = ' + c);
+        }
 
-        for (var i = 1; i <= CHART_RANGE; i++) {
-            rows.push([i]);
-            data.addColumn('number', 'r = ' + i);
+        for (var i = 1; i <= CHART_RANGE; i++){
+            n = i;
+            rows.push([n]);
 
-            m = i;
-
-            for (var j = 1; j <= CHART_RANGE; j++) {
-                n = j;
-
-                var A = generateMatrix(p, m);
-                var B = generateMatrix(m, q);
+            for (var j = 1; j <= CHART_RANGE; j++){
+                m = j;
                 var C = calculate(A, B);
-
                 rows[i - 1].push(calculateDivergency());
             }
         }
@@ -514,7 +489,8 @@ function drawChart5() {
     }
 }
 
-function drawChart6() {
+// D(r)
+function drawChart6(A, B) {
     google.charts.load('current', {'packages':['line']});
     google.charts.setOnLoadCallback(drawChart);
 
@@ -524,20 +500,18 @@ function drawChart6() {
         var rows = [];
 
         data.addColumn('number', 'r');
+        for (var c = 1; c <= CHART_RANGE; c++){
+            data.addColumn('number', 'n = ' + c);
+        }
 
-        for (var i = 1; i <= CHART_RANGE; i++) {
-            rows.push([i]);
-            data.addColumn('number', 'n = ' + i);
+        for (var i = 1; i <= CHART_RANGE; i++){
+            m = i;
+            rows.push([m]);
 
-            n = i;
+            for (var j = 1; j <= CHART_RANGE; j++){
+                n = j;
 
-            for (var j = 1; j <= CHART_RANGE; j++) {
-                m = j;
-
-                var A = generateMatrix(p, m);
-                var B = generateMatrix(m, q);
                 var C = calculate(A, B);
-
                 rows[i - 1].push(calculateDivergency());
             }
         }
